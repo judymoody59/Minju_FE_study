56955b9e70a2be01bff483038fd898bf2b5d1dae
judymoody59 <jmj.chai@gmail.com>
2024-07-11 18:00:56 +0900

    Feat: 로그인 페이지 구현

---
+++ src/Login.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+
+const User = {
+    email: 'kimkt@ewhain.net',
+    pw: 'ewha1234@@'
+}
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [pw, setPw] = useState('');
+
+    const [emailValid, setEmailValid] = useState(false);
+    const [pwValid, setPwValid] = useState(false);
+    const [notAllow, setNotAllow] = useState(false);
+ 
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        const regex = 
+            /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        if (regex.test(e.target.value)) {
+            setEmailValid(true);
+        }
+        else {
+            setEmailValid(false);
+        }
+    }
+
+    const handlePassword = (e) => {
+        setPw(e.target.value);
+        const regex = 
+            /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        if (regex.test(e.target.value)) {
+            setPwValid(true);
+        }
+        else {
+            setPwValid(false);
+        }
+    }
+
+    const onClickConfirmButton = () => {
+        if(email === User.email && pw === User.pw) {
+            alert('로그인에 성공했습니다.');
+        }
+        else if (email === User.email && pw != User.pw) {
+            alert('비밀번호가 맞지 않습니다.');
+        }
+        else {
+            alert('등록되지 않은 회원입니다.');
+        }
+    }
+
+    useEffect(() => {
+        if(emailValid && pwValid) {
+            setNotAllow(false);
+            return;
+        }
+        setNotAllow(true);
+    }, [emailValid, pwValid]);
+
+    return (
+        <div className="page">
+            <div className="titleWrap">
+                로그인
+                <br/>
+            </div>
+
+            <div className="contentWrap">
+                <div className="inputTitle">이메일 주소</div>
+                <div className="inputWrap">
+                    <input 
+                        type='text'
+                        className="input" 
+                        placeholder="user@example.com" 
+                        value={email}
+                        onChange={handleEmail}/>
+                </div>
+                <div className="errorMessageWrap">
+                    {
+                        !emailValid &&  email.length > 0 && (
+                            <div>올바른 이메일을 입력해주세요.</div>
+                        )
+                    }
+                </div>
+
+
+                <div style={{marginTop: "26px" }} className="inputTitle">비밀번호</div>
+                <div className="inputWrap">
+                    <input 
+                        type ='password'
+                        className="input"
+                        placeholder="password"
+                        value={pw}
+                        onChange={handlePassword}/>
+                </div>
+                <div className="errorMessageWrap">
+                    {
+                        !pwValid && pw.length > 0 && (
+                            <div> 영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+                        )
+                    }
+                </div>
+            </div>
+
+            <div>
+                <button onClick={onClickConfirmButton} disabled={notAllow} className="bottomButton">
+                    확인
+                </button>
+            </div>
+        </div>
+    )
+}
