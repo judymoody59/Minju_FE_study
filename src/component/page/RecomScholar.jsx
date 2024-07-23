b88f0caa91f231c37c52792d0564faecbacc6e92
judymoody59 <jmj.chai@gmail.com>
2024-07-23 21:38:28 +0900

    Feat: 전체 장학금, 추천 장학금 페이지에 네비게이션바 추가

--- src/component/page/RecomScholar.jsx
+++ src/component/page/RecomScholar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import NavBar from "../ui/NavBar";
 
 function RecomScholar(props) {
+    return (
+        <>
+            <NavBar/>
+        </>
+    );
 
 }
 export default RecomScholar;
